8194985802c8e4b7b8004e8b17ec088a260804d1
Ahmad Ali <Ahmad>
2024-02-03 03:53:05 +0300

    [design]: design the main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import  Navbar from "./navbar";
+import { Urbanist } from "next/font/google";
+
+const urbanist = Urbanist({weight: ["100", "200", "300", "400", "500", "700", "800", "900"], subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "Intelligence"
@@ -13,9 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`h-vh w-vw p-9 bg-modern-background`}>
-        <Navbar />
-        {children}
+      <body className={`${urbanist.className} flex flex-col h-lvh w-vw p-9 bg-modern-background`}>
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="children h-full">
+          {children}
+        </div>
       </body>
     </html>
   );
